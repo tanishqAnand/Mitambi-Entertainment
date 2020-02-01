6b99e2751a93ea1ee8cfaec291f8ab19235131e4
Tanishq Anand <tanishq0087@gmail.com>
2020-02-01 22:37:48 +0530

    changes to about_us.html and Debug=True

--- Mitambi_Entertainment/settings.py
+++ Mitambi_Entertainment/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-5gvzecs78#jlsvm^eg=_73r$hylh-m%36m-ujjs45u%$ra&3c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
